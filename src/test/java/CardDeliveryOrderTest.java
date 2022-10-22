ee0be53aa883b00ee548f6762efb03ec7433a902
Anna-St <106953540+Anna-Stankevich@users.noreply.github.com>
2022-10-22 12:18:19 +0300

    Update CardDeliveryOrderTest.java

--- src/test/java/CardDeliveryOrderTest.java
+++ src/test/java/CardDeliveryOrderTest.java
@@ -1,25 +1,22 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.*;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryOrderTest {
-    
+
     @BeforeEach
     void setupTest() {
         open("http://localhost:9999");
     }
-    
+
     String generateDate(int days){
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-    }   
-    
+    }
+
     @Test
     void shouldTestForm(){
         $("[data-test-id='city'] input").setValue("Казань");
@@ -31,7 +28,6 @@ public class CardDeliveryOrderTest {
         $("[data-test-id='phone'] input").setValue("+79000000000");
         $("[data-test-id='agreement']").click();
         $$("button").find(Condition.exactText("Забронировать")).click();
-        $("[data-test-id='notification'] button").shouldBe(Condition.visible, Duration.ofSeconds(15));
-        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15)).shouldBe(Condition.visible);
+        $("[data-test-id='notification'] button").shouldBe(Condition.visible, Duration.ofSeconds(20));
     }
 }
